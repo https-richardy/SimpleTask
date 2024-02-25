7a41570ab3282f869d979fdfcf7454c871ee34c1
https-richardy <=>
2024-02-25 12:27:05 -0300

    #feat: map get endpoints to recover all todos

--- Source/HttpsRichardy.SimpleTask.WebApi/Endpoints/TodoEndpoints.cs
+++ Source/HttpsRichardy.SimpleTask.WebApi/Endpoints/TodoEndpoints.cs
@@ -1,10 +1,25 @@
 using HttpsRichardy.SimpleTask.Application.Commands;
+using HttpsRichardy.SimpleTask.Application.Queries;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 public static class TodoEndpoints
 {
     public static void ConfigureTodoEndpoints(this IEndpointRouteBuilder endpoint)
     {
+        endpoint.MapGet("api/todos", async (IMediator mediator, [FromQuery] string? title = null, [FromQuery] bool IsCompleted = false) =>
+        {
+            # pragma warning disable CS8601
+            var request = new RetrieveAllTodosQuery
+            {
+                Title = title, /* CS8601: Possible null reference assignment. */
+                IsCompleted = IsCompleted
+            };
+
+            var response = await mediator.Send(request);
+            Results.Ok(response);
+        });
+
         endpoint.MapPost("api/todos", async (IMediator mediator, CreateTodoCommand request) =>
         {
             await mediator.Send(request);

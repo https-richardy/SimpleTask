03157b378e65b8a1734e4d82fe1b3ac6c6a49647
https-richardy <=>
2024-02-25 20:43:53 -0300

    #chore: remove blank line

--- Source/HttpsRichardy.SimpleTask.WebApi/Endpoints/ApplicationEndpoints.cs
+++ Source/HttpsRichardy.SimpleTask.WebApi/Endpoints/ApplicationEndpoints.cs
@@ -3,7 +3,6 @@ public static class ApplicationEndpoints
     public static void ConfigureEndpoints(this IApplicationBuilder app)
     {
         app.UseRouting();
-
         app.UseEndpoints(endpoints =>
         {
             TodoEndpoints.ConfigureTodoEndpoints(endpoints);

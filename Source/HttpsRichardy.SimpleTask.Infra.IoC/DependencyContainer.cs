d0e1e3fbe4d2c75275ac1b98e1a80ceea4c2d602
https-richardy <=>
2024-02-25 13:42:50 -0300

    #feat: validation extension registered in the dependency container

--- Source/HttpsRichardy.SimpleTask.Infra.IoC/DependencyContainer.cs
+++ Source/HttpsRichardy.SimpleTask.Infra.IoC/DependencyContainer.cs
@@ -11,6 +11,7 @@ public static class DependencyContainer
         services.AddDataPersistence(configuration);
 
         services.AddMediator();
+        services.AddValidation();
         services.AddMapping();
     }
 }

c62fd06fede84ea84702a1d3d2636cb98f11d07c
https-richardy <=>
2024-02-25 15:06:10 -0300

    #feat: create objectdoesnotexist middleware

---
+++ Source/HttpsRichardy.SimpleTask.WebApi/Middlewares/ObjectDoesNotExistMiddleware/ObjectDoesNotExistExtension.cs
@@ -0,0 +1,9 @@
+namespace HttpsRichardy.SimpleTask.WebApi.Middlewares;
+
+public static class ObjectDoesNotExistExceptionMiddlewareExtensions
+{
+    public static IApplicationBuilder UseObjectDoesNotExistExceptionHandler(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ObjectDoesNotExistExceptionMiddleware>();
+    }
+}

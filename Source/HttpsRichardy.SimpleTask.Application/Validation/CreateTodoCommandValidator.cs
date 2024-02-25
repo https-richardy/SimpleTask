be15943e5af5d66c946efb1e6a5d85ac3a1963ef
https-richardy <=>
2024-02-25 13:23:47 -0300

    #feat: create todo command validator

---
+++ Source/HttpsRichardy.SimpleTask.Application/Validation/CreateTodoCommandValidator.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+using HttpsRichardy.SimpleTask.Application.Commands;
+
+namespace HttpsRichardy.SimpleTask.Application.Validation;
+
+public class CreateTodoCommandValidator : AbstractValidator<CreateTodoCommand>, IValidator<CreateTodoCommand>
+{
+    public CreateTodoCommandValidator()
+    {
+        RuleFor(command => command.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(100).WithMessage("Title must not exceed 100 characters.");
+
+        RuleFor(command => command.Description)
+            .MaximumLength(180).WithMessage("Description must not exceed 180 charactes");
+
+        RuleFor(command => command.DueDate)
+            .Must(date => date == null|| date >= DateTime.UtcNow)
+            .WithMessage("Due date must be in the future, or not provided.");
+
+        RuleFor(command => command.Priority)
+            .IsInEnum().WithMessage("Invalid priority value.");
+    }
+}

58a55e60937b9545571f6d9c2c6ce531a37f3122
https-richardy <=>
2024-02-25 10:43:09 -0300

    #feat: added dependency container

---
+++ Source/HttpsRichardy.SimpleTask.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+using HttpsRichardy.SimpleTask.Infra.IoC.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HttpsRichardy.SimpleTask.Infra.IoC;
+
+public static class DependencyContainer
+{
+    public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDataPersistence(configuration);
+        services.AddMediator();
+    }
+}

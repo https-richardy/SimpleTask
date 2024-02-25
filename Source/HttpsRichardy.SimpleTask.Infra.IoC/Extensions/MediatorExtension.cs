5c016f1f8507aebfe44f00473b0a2ef24bcf5916
https-richardy <=>
2024-02-25 12:25:47 -0300

    #feat: retrieve all todos query registered in dependencies container

--- Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/MediatorExtension.cs
+++ Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/MediatorExtension.cs
@@ -1,6 +1,9 @@
 using System.Reflection;
 using HttpsRichardy.SimpleTask.Application.Commands;
 using HttpsRichardy.SimpleTask.Application.Commands.Handlers;
+using HttpsRichardy.SimpleTask.Application.Queries;
+using HttpsRichardy.SimpleTask.Application.Queries.Handlers;
+using HttpsRichardy.SimpleTask.Domain.Models;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,5 +16,6 @@ public static class MediatorExtension
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
 
         services.AddTransient<IRequestHandler<CreateTodoCommand, CreateTodoResponse>, CreateTodoCommandHandler>();
+        services.AddTransient<IRequestHandler<RetrieveAllTodosQuery, IEnumerable<ToDo>>, RetrieveAllTodosQueryHandler>();
     }
 }

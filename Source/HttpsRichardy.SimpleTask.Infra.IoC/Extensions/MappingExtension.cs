190483e7289100acd0d500427c5e5250d5ad7bf6
https-richardy <=>
2024-02-25 13:35:47 -0300

    #feat: mapping of the create todo command to a configured todo model

---
+++ Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/MappingExtension.cs
@@ -0,0 +1,20 @@
+using HttpsRichardy.SimpleTask.Application.Commands;
+using HttpsRichardy.SimpleTask.Domain.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Nelibur.ObjectMapper;
+
+namespace HttpsRichardy.SimpleTask.Infra.IoC.Extensions;
+
+public static class MappingExtension
+{
+    public static void AddMapping(this IServiceCollection services)
+    {
+        TinyMapper.Bind<CreateTodoCommand, ToDo>(cfg =>
+        {
+            cfg.Bind(src => src.Title, target => target.Title);
+            cfg.Bind(src => src.Description, target => target.Description);
+            cfg.Bind(src => src.DueDate, target => target.DueDate);
+            cfg.Bind(src => src.Priority, target => target.Priority);
+        });
+    }
+}

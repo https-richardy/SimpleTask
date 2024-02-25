85c92692ed82a2b57067fb09334f0f82c224c4cf
https-richardy <=>
2024-02-25 14:35:30 -0300

    #feat: added retrieve todo by id query request

---
+++ Source/HttpsRichardy.SimpleTask.Application/Queries/Requests/RetrieveTodoByIdQuery.cs
@@ -0,0 +1,9 @@
+using HttpsRichardy.SimpleTask.Domain.Models;
+using MediatR;
+
+namespace HttpsRichardy.SimpleTask.Application.Queries;
+
+public record RetrieveTodoByIdQuery : IRequest<ToDo>
+{
+    public int Id { get; init; }
+}

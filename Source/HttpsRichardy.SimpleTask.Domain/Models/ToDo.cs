c78bf0d093addc4a4cf44ef86ce7e7c27009b2c1
https-richardy <=>
2024-02-25 13:14:17 -0300

    #feat: improvie todo model

--- Source/HttpsRichardy.SimpleTask.Domain/Models/ToDo.cs
+++ Source/HttpsRichardy.SimpleTask.Domain/Models/ToDo.cs
@@ -1,7 +1,12 @@
+using HttpsRichardy.SimpleTask.Domain.Models.Enums;
+
 namespace HttpsRichardy.SimpleTask.Domain.Models;
 
 public record ToDo : Model
 {
     public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    public DateTime? DueDate { get; set; }
     public bool Done { get; set; } = false;
+    public Priority Priority { get; set; }
 }

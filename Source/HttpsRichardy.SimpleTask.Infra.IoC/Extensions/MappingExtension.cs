37d6eb11718e000845fe968bef92e1e6be79d159
https-richardy <=>
2024-02-25 15:40:28 -0300

    #feat: added mapping from an UpdateTodoCommand to a ToDo

--- Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/MappingExtension.cs
+++ Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/MappingExtension.cs
@@ -16,5 +16,13 @@ public static class MappingExtension
             cfg.Bind(src => src.DueDate, target => target.DueDate);
             cfg.Bind(src => src.Priority, target => target.Priority);
         });
+
+        TinyMapper.Bind<UpdateTodoCommand, ToDo>(cfg =>
+        {
+            cfg.Bind(src => src.Title, target => target.Title);
+            cfg.Bind(src => src.Description, target => target.Description);
+            cfg.Bind(src => src.DueDate, target => target.DueDate);
+            cfg.Bind(src => src.Priority, target => target.Priority);
+        });
     }
 }

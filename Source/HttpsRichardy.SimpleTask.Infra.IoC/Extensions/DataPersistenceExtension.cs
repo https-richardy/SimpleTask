53cc0258f4a5cacdc90bb39aee11c62d14fed0aa
https-richardy <=>
2024-02-25 10:42:57 -0300

    #feat: added data persistence extension

---
+++ Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/DataPersistenceExtension.cs
@@ -0,0 +1,24 @@
+using HttpsRichardy.SimpleTask.Domain.Contracts.Repositories;
+using HttpsRichardy.SimpleTask.Domain.Models;
+using HttpsRichardy.SimpleTask.Infra.Data;
+using HttpsRichardy.SimpleTask.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HttpsRichardy.SimpleTask.Infra.IoC.Extensions;
+
+public static class DataPersistenceExtension
+{
+    public static void AddDataPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Default");
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+
+        services.AddScoped<IRepository<ToDo>, Repository<ToDo>>();
+    }
+}

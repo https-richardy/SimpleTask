19d273f782a0fcca37dcc3030f1330142a14d132
https-richardy <=>
2024-02-25 14:07:19 -0300

    #feat: added new migrations for new ToDo Model fields

---
+++ Source/HttpsRichardy.SimpleTask.Infra/Data/Migrations/20240225170605_NewFieldsForTodoModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HttpsRichardy.SimpleTask.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewFieldsForTodoModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "ToDos",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "ToDos",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "ToDos",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "ToDos");
+
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "ToDos");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "ToDos");
+        }
+    }
+}

52118cd10cede8c992128575cadca375157ebbf8
https-richardy <=>
2024-02-28 15:40:48 -0300

    #feat: registered authentication and authorization middleware.

--- Source/HttpsRichardy.SimpleTask.WebApi/Endpoints/ApplicationEndpoints.cs
+++ Source/HttpsRichardy.SimpleTask.WebApi/Endpoints/ApplicationEndpoints.cs
@@ -3,6 +3,10 @@ public static class ApplicationEndpoints
     public static void ConfigureEndpoints(this IApplicationBuilder app)
     {
         app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
         app.UseEndpoints(endpoints =>
         {
             TodoEndpoints.ConfigureTodoEndpoints(endpoints);

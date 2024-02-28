557e44d1228a761b2a151e6dc525f931315a2ff2
https-richardy <=>
2024-02-28 13:27:56 -0300

    #feat: added application user model

---
+++ Source/HttpsRichardy.SimpleTask.Infra/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using HttpsRichardy.SimpleTask.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace HttpsRichardy.SimpleTask.Infra.Identity;
+
+public class ApplicationUser : IdentityUser, IUser
+{
+    
+}

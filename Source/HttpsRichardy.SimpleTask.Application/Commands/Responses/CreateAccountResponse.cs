903f0c2ad14bb4b236daa26e68db1ef4af908c1b
https-richardy <=>
2024-02-28 14:26:22 -0300

    #remove: removed duplicate from CreateAccountResponse

--- Source/HttpsRichardy.SimpleTask.Application/Commands/Responses/CreateAccountResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace HttpsRichardy.SimpleTask.Application.AccountContext.Commands.Responses;
-
-public record CreateAccountResponse
-{
-    public bool Success { get; init; }
-    public string Message { get; init; } = string.Empty;
-    public object? Errors { get; init; }
-}

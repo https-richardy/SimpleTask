03e7909679cec26c8020c577b3291702425fadaa
https-richardy <=>
2024-02-28 15:13:55 -0300

    #chore: adjustment registered the ApplicationUser in the IdentityDbContext

--- Source/HttpsRichardy.SimpleTask.Infra/Data/AppDbContext.cs
+++ Source/HttpsRichardy.SimpleTask.Infra/Data/AppDbContext.cs
@@ -1,10 +1,11 @@
 using HttpsRichardy.SimpleTask.Domain.Models;
+using HttpsRichardy.SimpleTask.Infra.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HttpsRichardy.SimpleTask.Infra.Data;
 
-public class AppDbContext : IdentityDbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser>
 {
     public DbSet<ToDo> ToDos { get; set; }
 

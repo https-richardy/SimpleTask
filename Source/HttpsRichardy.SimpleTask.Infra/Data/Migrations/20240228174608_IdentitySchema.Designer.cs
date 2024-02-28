d232b47878e92ecff8c682a3669ea5e07cf4f0f3
https-richardy <=>
2024-02-28 14:47:40 -0300

    #feat: added new migrations. IdentitySchema has been added

--- Source/HttpsRichardy.SimpleTask.Infra/Data/Migrations/20240228174608_IdentitySchema.Designer.cs
+++ Source/HttpsRichardy.SimpleTask.Infra/Data/Migrations/20240228174608_IdentitySchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HttpsRichardy.SimpleTask.Infra.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HttpsRichardy.SimpleTask.Infra.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240228174608_IdentitySchema")]
+    partial class IdentitySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.2");

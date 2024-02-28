086e85cec18246bf2fd5763fc04784ca3f64d6bf
https-richardy <=>
2024-02-28 15:45:47 -0300

    #feat: added user interface for navigation and relationship

--- Source/HttpsRichardy.SimpleTask.Domain/Models/ToDo.cs
+++ Source/HttpsRichardy.SimpleTask.Domain/Models/ToDo.cs
@@ -2,11 +2,13 @@ using HttpsRichardy.SimpleTask.Domain.Models.Enums;
 
 namespace HttpsRichardy.SimpleTask.Domain.Models;
 
+# pragma warning disable CS8618
 public record ToDo : Model
 {
     public string Title { get; set; } = string.Empty;
     public string? Description { get; set; } = string.Empty;
     public DateTime? DueDate { get; set; }
     public bool Done { get; set; } = false;
+    public IUser User { get; set; }
     public Priority? Priority { get; set; }
 }

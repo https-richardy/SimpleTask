9b91c98a4604f1fe64aa0c2a6c6cd2d49c76fb13
https-richardy <=>
2024-02-28 13:54:18 -0300

    #feat: added security extension for authentication 'n authorization

---
+++ Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/SecurityExtension.cs
@@ -0,0 +1,32 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+public static class SecurityExtension
+{
+    private static void AddSecurity(this IServiceCollection services, IConfiguration configuration)
+    {
+        # pragma warning disable CS8604
+        var secretKey = Encoding.ASCII.GetBytes(configuration["Jwt:SecretKey"]); /* Possible null reference argument */
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(secretKey),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.FromMinutes(5)
+            };
+        });
+
+        services.AddAuthorization();
+    }
+}

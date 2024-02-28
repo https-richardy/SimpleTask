3aaf3d519ab69b25f506b8b6cb9259c4a927ffa3
https-richardy <=>
2024-02-28 18:11:55 -0300

    #feat: Added Todos property to the ApplicationUser class to establish a relationship for EntityTypeConfiguration, allowing users to have multiple Todos.

--- Source/HttpsRichardy.SimpleTask.Infra/Identity/ApplicationUser.cs
+++ Source/HttpsRichardy.SimpleTask.Infra/Identity/ApplicationUser.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Identity;
 
 namespace HttpsRichardy.SimpleTask.Infra.Identity;
 
+# pragma warning disable CS8618
 public class ApplicationUser : IdentityUser, IUser
 {
-    
+    public ICollection<ToDo> Todos { get; set; }
 }

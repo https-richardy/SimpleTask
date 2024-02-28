ed837785bd3d9f6313b9ce840708d5869d809e49
https-richardy <=>
2024-02-28 18:39:18 -0300

    #feat: Introduced new migrations to establish a relationship between a user and multiple ToDos in the database schema.

---
+++ Source/HttpsRichardy.SimpleTask.Infra/Data/Migrations/20240228213750_RelationshipBetweenTodoAndUser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HttpsRichardy.SimpleTask.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationshipBetweenTodoAndUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "ToDos",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDos_UserId",
+                table: "ToDos",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ToDos_AspNetUsers_UserId",
+                table: "ToDos",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ToDos_AspNetUsers_UserId",
+                table: "ToDos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ToDos_UserId",
+                table: "ToDos");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "ToDos");
+        }
+    }
+}

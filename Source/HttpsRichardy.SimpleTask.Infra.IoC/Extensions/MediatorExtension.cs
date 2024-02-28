3b5d1b140cef8ed44080100d27dc66c9424152ac
https-richardy <=>
2024-02-28 14:34:32 -0300

    #feat: commands, queries and handlers for account creation and authentication were registered in the DI container

--- Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/MediatorExtension.cs
+++ Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/MediatorExtension.cs
@@ -1,4 +1,9 @@
 using System.Reflection;
+using HttpsRichardy.SimpleTask.Application.AccountContext.Commands;
+using HttpsRichardy.SimpleTask.Application.AccountContext.Commands.Handlers;
+using HttpsRichardy.SimpleTask.Application.AccountContext.Commands.Responses;
+using HttpsRichardy.SimpleTask.Application.AccountContext.Queries;
+using HttpsRichardy.SimpleTask.Application.AccountContext.Queries.Responses;
 using HttpsRichardy.SimpleTask.Application.Commands;
 using HttpsRichardy.SimpleTask.Application.Commands.Handlers;
 using HttpsRichardy.SimpleTask.Application.Queries;
@@ -22,5 +27,12 @@ public static class MediatorExtension
 
         services.AddTransient<IRequestHandler<RetrieveAllTodosQuery, IEnumerable<ToDo>>, RetrieveAllTodosQueryHandler>();
         services.AddTransient<IRequestHandler<RetrieveTodoByIdQuery, ToDo>, RetrieveTodoByIdQueryHandler>();
+
+        # region Account Mediators
+
+        services.AddTransient<IRequestHandler<CreateAccountCommand, CreateAccountResponse>, CreateAccountCommandHandler>();
+        services.AddTransient<IRequestHandler<AuthenticationQuery, AuthenticationQueryResponse>, AuthenticationQueryHandler>();
+
+        # endregion
     }
 }

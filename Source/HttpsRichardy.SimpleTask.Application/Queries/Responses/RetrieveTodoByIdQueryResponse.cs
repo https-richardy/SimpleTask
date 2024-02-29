b118807385f8d452936157e6b3a1a734f6a0a84b
https-richardy <=>
2024-02-29 18:20:58 -0300

    #feat: introduce retrieve todo by id query response for relevant data

---
+++ Source/HttpsRichardy.SimpleTask.Application/Queries/Responses/RetrieveTodoByIdQueryResponse.cs
@@ -0,0 +1,13 @@
+using HttpsRichardy.SimpleTask.Domain.Models.Enums;
+
+namespace HttpsRichardy.SimpleTask.Application.Queries.Responses;
+
+public record RetrieveTodoByIdQueryResponse
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    public DateTime? DueDate { get; set; }
+    public bool Done { get; set; } = false;
+    public Priority? Priority { get; set; }
+}

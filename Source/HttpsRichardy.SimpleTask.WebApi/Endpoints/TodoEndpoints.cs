e3affba350a4fe1fe4a3ed1699223fd8c8d9664c
https-richardy <=>
2024-02-29 10:31:15 -0300

    #chore: adjusted TodoConfiguration to utilize the Todo.UserId property directly instead of specifying the foreign key string.

--- Source/HttpsRichardy.SimpleTask.WebApi/Endpoints/TodoEndpoints.cs
+++ Source/HttpsRichardy.SimpleTask.WebApi/Endpoints/TodoEndpoints.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using HttpsRichardy.SimpleTask.Application.Commands;
 using HttpsRichardy.SimpleTask.Application.Queries;
 using MediatR;
@@ -8,29 +9,32 @@ public static class TodoEndpoints
 {
     public static void ConfigureTodoEndpoints(this IEndpointRouteBuilder endpoint)
     {
-        endpoint.MapGet("api/todos", async (IMediator mediator, [FromQuery] string? title = null, [FromQuery] bool IsCompleted = false) =>
+        endpoint.MapGet("api/todos", [Authorize] async (IMediator mediator, ClaimsPrincipal user, [FromQuery] string? title = null, [FromQuery] bool IsCompleted = false) =>
         {
             # pragma warning disable CS8601
             var request = new RetrieveAllTodosQuery
             {
                 Title = title, /* CS8601: Possible null reference assignment. */
-                IsCompleted = IsCompleted
+                IsCompleted = IsCompleted,
+                UserId = user.FindFirst(ClaimTypes.NameIdentifier)?.Value
             };
 
             var response = await mediator.Send(request);
             return Results.Ok(response);
         });
 
-        endpoint.MapGet("api/todos/{id}", async (IMediator mediator, [FromRoute] int id) =>
+        endpoint.MapGet("api/todos/{id}", async (IMediator mediator, [FromRoute] int id, ClaimsPrincipal user) =>
         {
-            var request = new RetrieveTodoByIdQuery { Id = id };
+            var request = new RetrieveTodoByIdQuery { Id = id, UserId = user.FindFirst(ClaimTypes.NameIdentifier)?.Value };
 
             var response = await mediator.Send(request);
             return response != null ? Results.Ok(response) : Results.NotFound();
         });
 
-        endpoint.MapPost("api/todos", [Authorize] async (IMediator mediator, CreateTodoCommand request) =>
+        endpoint.MapPost("api/todos", [Authorize] async (IMediator mediator, CreateTodoCommand request, ClaimsPrincipal user) =>
         {
+            request.UserId = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
             await mediator.Send(request);
             return Results.Created();
         });

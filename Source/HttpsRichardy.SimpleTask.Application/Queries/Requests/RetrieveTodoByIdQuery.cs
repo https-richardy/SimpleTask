44962c8f0acb45d22926f3e9c2e5e1bccc8354f5
https-richardy <=>
2024-02-29 18:21:33 -0300

    #refactor: refactored RetrieveTodoByIdQuery to use RetrieveTodoByIdQueryResponse as its response type.

--- Source/HttpsRichardy.SimpleTask.Application/Queries/Requests/RetrieveTodoByIdQuery.cs
+++ Source/HttpsRichardy.SimpleTask.Application/Queries/Requests/RetrieveTodoByIdQuery.cs
@@ -1,9 +1,9 @@
-using HttpsRichardy.SimpleTask.Domain.Models;
+using HttpsRichardy.SimpleTask.Application.Queries.Responses;
 using MediatR;
 
 namespace HttpsRichardy.SimpleTask.Application.Queries;
 
-public record RetrieveTodoByIdQuery : IRequest<ToDo>
+public record RetrieveTodoByIdQuery : IRequest<RetrieveTodoByIdQueryResponse>
 {
     public int Id { get; init; }
     public string UserId { get; init; } = string.Empty;

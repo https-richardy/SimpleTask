46b7210e249d54d9b37928cca9ea3f951c842786
https-richardy <=>
2024-02-29 18:22:45 -0300

    #feat: enhanced RetrieveAllTodosQueryResponse by adding the 'Id' property, providing clients with relevant task identifiers.

--- Source/HttpsRichardy.SimpleTask.Application/Queries/Responses/RetrieveAllTodosQueryResponse.cs
+++ Source/HttpsRichardy.SimpleTask.Application/Queries/Responses/RetrieveAllTodosQueryResponse.cs
@@ -4,6 +4,7 @@ namespace HttpsRichardy.SimpleTask.Application.Queries.Responses;
 
 public record RetrieveAllTodosQueryResponse
 {
+    public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string? Description { get; set; } = string.Empty;
     public DateTime? DueDate { get; set; }

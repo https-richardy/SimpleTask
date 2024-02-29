845692febb2eca064889738b7561875671facf80
https-richardy <=>
2024-02-29 18:24:58 -0300

    #refactor: refactored the registration of the `RetrieveTodoByIdQueryResponse` handler in the service collection to use the more specific `RetrieveTodoByIdQueryResponse` as its response type.

--- Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/MediatorExtension.cs
+++ Source/HttpsRichardy.SimpleTask.Infra.IoC/Extensions/MediatorExtension.cs
@@ -9,7 +9,6 @@ using HttpsRichardy.SimpleTask.Application.Commands.Handlers;
 using HttpsRichardy.SimpleTask.Application.Queries;
 using HttpsRichardy.SimpleTask.Application.Queries.Handlers;
 using HttpsRichardy.SimpleTask.Application.Queries.Responses;
-using HttpsRichardy.SimpleTask.Domain.Models;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -27,7 +26,7 @@ public static class MediatorExtension
         services.AddTransient<IRequestHandler<DeleteTodoCommand>, DeleteTodoCommandHandler>();
 
         services.AddTransient<IRequestHandler<RetrieveAllTodosQuery, IEnumerable<RetrieveAllTodosQueryResponse>>, RetrieveAllTodosQueryHandler>();
-        services.AddTransient<IRequestHandler<RetrieveTodoByIdQuery, ToDo>, RetrieveTodoByIdQueryHandler>();
+        services.AddTransient<IRequestHandler<RetrieveTodoByIdQuery, RetrieveTodoByIdQueryResponse>, RetrieveTodoByIdQueryHandler>();
 
         # region Account Mediators
 

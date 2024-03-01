3b9a85c391d89890aeb5eea22a99f1f5aacb393c
https-richardy <=>
2024-03-01 11:46:09 -0300

    #feat: Include 'UserId' in CompleteTodoCommand. Enhanced the CompleteTodoCommand by including the UserId property to capture the user's ID who initiated the request.

--- Source/HttpsRichardy.SimpleTask.Application/Commands/Requests/CompleteTodoCommand.cs
+++ Source/HttpsRichardy.SimpleTask.Application/Commands/Requests/CompleteTodoCommand.cs
@@ -5,4 +5,5 @@ namespace HttpsRichardy.SimpleTask.Application.Commands;
 public record CompleteTodoCommand : IRequest
 {
     public int TodoId { get; init; }
+    public string UserId { get; init; } = string.Empty;
 }

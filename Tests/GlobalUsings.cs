5651550fbe093642bd365e46ea7c7d304328eb37
https-richardy <=>
2024-03-01 14:35:35 -0300

    #feat: updated global usings

--- Tests/GlobalUsings.cs
+++ Tests/GlobalUsings.cs
@@ -1,8 +1,22 @@
 #region -- Project global usings --
 
 global using HttpsRichardy.SimpleTask.Domain.TodoContext.Models;
+global using HttpsRichardy.SimpleTask.Infra.Data;
+global using HttpsRichardy.SimpleTask.Domain.TodoContext.Contracts.Repositories;
+global using HttpsRichardy.SimpleTask.Infra.Data.Repositories;
 
 #endregion
 
 
+#region  -- Microsoft global usings --
+
+global using Microsoft.EntityFrameworkCore;
+
+#endregion
+
+#region  -- Thirdy part global usings --
+
 global using Xunit;
+global using AutoFixture;
+
+#endregion

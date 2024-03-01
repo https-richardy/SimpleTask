5fa53c784abb82ebdb22740ad14efeb3de73baf9
https-richardy <=>
2024-03-01 12:00:42 -0300

    #feat: Register UnauthorizedExceptionMiddleware in Application Pipeline.

--- Source/HttpsRichardy.SimpleTask.WebApi/Program.cs
+++ Source/HttpsRichardy.SimpleTask.WebApi/Program.cs
@@ -24,6 +24,7 @@ internal class Program
 
         app.UseObjectDoesNotExistExceptionHandler();
         app.UseValidationExceptionHandler();
+        app.UseUnauthorizedExceptionMiddlewareHandler();
 
         app.ConfigureEndpoints();
         app.UseHttpsRedirection();
